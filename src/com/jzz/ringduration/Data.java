9e95ef91255a728233a3f90981006bacf30038f8
jzz4012650 <jzz4012650@qq.com>
2014-10-17 23:32:44 +0800

    added verion 1.0

---
+++ src/com/jzz/ringduration/Data.java
@@ -0,0 +1,15 @@
+package com.jzz.ringduration;
+
+/**
+ * ´æ´¢¼òµ¥Êý¾Ý
+ * @author jinzhenzu
+ *
+ */
+public class Data {
+	
+	public static boolean serviceRunning;
+	public static boolean incoming;
+	public static String incomingNumber;
+	public static long durationTime;
+	
+}
